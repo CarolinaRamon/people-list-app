510aaec367fd65ddc619e30799d0a39841e2d9e9
Carolina <37455674+CarolinaRamon@users.noreply.github.com>
2021-08-31 20:10:59 -0300

    Created Class Person

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 console.log('People List App');
+
+const persons =  [
+    new Person('John', 'Smith'),
+    new Person('Jane', 'Doe'),
+];
+
+function showPeople(){
+    console.log('Showing people...');
+    let texto = '';
+    for(let person of persons){
+        console.log(person);
+        texto += `<li>${person.name} ${person.surname}</li>`
+    }
+    document.getElementById('persons').innerHTML = texto;
+}
